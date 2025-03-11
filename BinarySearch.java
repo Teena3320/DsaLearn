ce909b75d1939a0df74f124a0ba7f0b60b0d01af
Teena3320 <teena.sebastian2022@vitstudent.ac.in>
2025-03-11 09:59:30 +0530

    Ceiling and floor of Binary search

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,9 +1,12 @@
 public class BinarySearch {
   public static void main(String[] args) {
-    int[] arr={-18,-12,-4,0,2,3,4,15,16,18,22,45,89};//sorted
-    int target=42;
+    int[] arr={-4,0,2,3,16,18,22,45};//sorted
+    int target=-21;
     int ans=binarySearch(arr,target);
-    System.out.println(ans);
+    int ans1=ceilingBS(arr, target);
+    int ans2=floorBS(arr, target);
+    System.out.println(ans +" "+ ans1 +" "+ans2 );
+
   }
   static int binarySearch(int[] arr, int target){
     int start=0;
@@ -20,6 +23,42 @@ public class BinarySearch {
       return mid;    
     }
     return -1;
+  }
+
+  static int ceilingBS(int[] arr ,int target){
+    if(target > arr[arr.length-1]){ //no num greater than target
+      return -1;
+    }
+    int start=0;
+    int end=arr.length-1;
+    while(start<=end){
+      int mid=start+(end-start)/2;
+      if(target< arr[mid]){
+        end=mid-1;
+      }
+      else if(target> arr[mid]){
+        start=mid+1;
+      }
+      else
+      return mid;    
+    }
+    return start;
+  }
+  static int floorBS(int[] arr ,int target){
+    int start=0;
+    int end=arr.length-1;
+    while(start<=end){
+      int mid=start+(end-start)/2;
+      if(target< arr[mid]){
+        end=mid-1;
+      }
+      else if(target> arr[mid]){
+        start=mid+1;
+      }
+      else
+      return mid;    
+    }
+    return end;
   }
 }
  
